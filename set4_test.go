5ecffd311d30f0c56d51593a6fde0517852aa437
Mohit Muthanna Cheppudira <mohit@muthanna.com>
2020-05-13 08:08:08 -0400

    Finished Set 4, Challenge 26!

--- set4_test.go
+++ set4_test.go
@@ -3,9 +3,12 @@ package cryptopals
 import (
 	"bytes"
 	"encoding/base64"
+	"fmt"
 	"io/ioutil"
 	"math/rand"
+	"regexp"
 	"testing"
+	"time"
 )
 
 func TestS4C25(t *testing.T) {
@@ -56,4 +59,74 @@ func TestS4C25(t *testing.T) {
 	}
 
 	assertTrue(t, bytes.Equal(plainText, newPlainText))
+}
+
+func TestS4C26(t *testing.T) {
+	rand.Seed(time.Now().UnixNano())
+	key := make([]byte, 16)
+	_, err := rand.Read(key)
+	assertNoError(t, err)
+	nonce := uint64(rand.Int63())
+	pre := []byte("comment1=cooking%20MCs;userdata=")
+	post := []byte(";comment2=%20like%20a%20pound%20of%20bacon")
+
+	encrypt := func(input []byte) ([]byte, error) {
+		sanitizedInput := []byte{}
+		for _, c := range input {
+			if c != ';' && c != '=' {
+				sanitizedInput = append(sanitizedInput, c)
+			}
+		}
+
+		plainText := append(pre, append(sanitizedInput, post...)...)
+
+		cipherText, err := encryptAESCTR(plainText, key, nonce)
+		if err != nil {
+			return nil, fmt.Errorf("could not CTR encrypt: %w", err)
+		}
+
+		return cipherText, nil
+	}
+
+	decrypt := func(cipherText []byte) ([]byte, error) {
+		plainText, err := decryptAESCTR(cipherText, key, nonce)
+		if err != nil {
+			return nil, fmt.Errorf("could not CBC decrypt: %w", err)
+		}
+
+		return plainText, nil
+	}
+
+	isCracked := func(cipherText []byte) bool {
+		plainText, err := decrypt(cipherText)
+		assertNoError(t, err)
+
+		match, err := regexp.MatchString(";admin=true;", string(plainText))
+		assertNoError(t, err)
+
+		return match
+	}
+
+	cipherText, err := encrypt([]byte(";admin=true;"))
+	assertNoError(t, err)
+	assertFalse(t, isCracked(cipherText))
+
+	// Flips a single bit in data, indexed by byteIndex and bitIndex
+	flipBit := func(data []byte, byteIndex int, bitIndex int) {
+		data[byteIndex] ^= byte((1 << 7) >> bitIndex)
+	}
+
+	adminBlock := []byte(";admin=true;")
+	flipBit(adminBlock, 0, 7)
+	flipBit(adminBlock, 6, 7)
+	flipBit(adminBlock, 11, 7)
+
+	attackBlock := append(pre, append(adminBlock, post...)...)
+	cipherText, err = encrypt(adminBlock)
+	assertEquals(t, len(attackBlock), len(cipherText))
+
+	flipBit(cipherText, 32, 7)
+	flipBit(cipherText, 32+6, 7)
+	flipBit(cipherText, 32+11, 7)
+	assertTrue(t, isCracked(cipherText))
 }
